282cea0a4f097dade693cf249177af64d16c9b14
buingoctai <buingoctai994@gmail.com>
2020-08-23 17:59:42 +0700

    context api & loops usage

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ContextApi from "./ContextApi";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ContextApi />
+    </div>
+  );
+}
+
+export default App;
